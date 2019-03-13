c8b2e66ad7e2f72e3e5341dd1d66efa3749e0e52
VladimirZhdanov <VladimirZhdanov1992@gmail.com>
2019-03-13 17:10:07 +0600

    Serve static files

--- handlers.js
+++ handlers.js
@@ -2,36 +2,61 @@ const fs = require("fs");
 const path = require("path");
 
 const about = (req, res) => {
-  res.end("<h1>About</h1>");
+    res.end("<h1>About</h1>");
 };
 
 const index = (req, res) => {
-  res.end("<h1>Index</h1>");
+    res.end("<h1>Index</h1>");
 };
 
 const error404 = (req, res) => {
-  res.end("<h1>404 Not Found!</h1>");
+    res.end("<h1>404 Not Found!</h1>");
 };
 
 const staticImage = (req, res) => {
-  const fstream = fs.createReadStream(
-    path.join(process.cwd(), "./static/ima.jpg")
-  );
-  fstream.pipe(res);
-  fstream.on("data", chunk => {
-    console.log(chunk);
-  });
-  fstream.on("end", () => {
-    res.end();
-  });
-  fstream.on("error", () => {
-    res.end("=(");
-  });
+    console.log(req.URL.pathname);
+
+    const fstream = fs.createReadStream(
+        path.join(process.cwd(), "./static/images/image.jpg")
+    );
+    fstream.pipe(res);
+    fstream.on("data", chunk => {
+        console.log(chunk);
+    });
+    fstream.on("end", () => {
+        res.end();
+    });
+    fstream.on("error", () => {
+        res.end("=(");
+    });
+};
+
+const staticFiles = (req, res) => {
+    console.log(req.URL.pathname);
+    abs_path = path.join(process.cwd(), req.URL.pathname);
+    fs.stat(abs_path, (err, fs_stats) => {
+        if (err) {
+            error404(req, res);
+        } else {
+            const fstream = fs.createReadStream(abs_path);
+            fstream.pipe(res);
+            fstream.on("data", chunk => {
+                console.log(chunk);
+            });
+            fstream.on("end", () => {
+                res.end();
+            });
+            fstream.on("error", () => {
+                res.end("=(");
+            });
+        }
+    });
 };
 
 module.exports = {
-  about,
-  index,
-  error404,
-  staticImage
+    about,
+    index,
+    error404,
+    staticImage,
+    staticFiles
 };
--- server.js
+++ server.js
@@ -3,47 +3,59 @@ const url = require("url");
 const querystring = require("querystring");
 //
 const winston = require("winston");
-const { index, about, error404, staticImage } = require("./handlers.js");
+const {
+    index,
+    about,
+    error404,
+    staticImage,
+    staticFiles
+} = require("./handlers.js");
 
 const PORT = 8000;
 
 const logger = winston.createLogger({
-  level: "debug",
-  transports: [new winston.transports.Console()]
+    level: "debug",
+    transports: [new winston.transports.Console()]
 });
 
 const server = http.createServer();
 
 server.on("connection", () => {
-  logger.info("New connection");
+    logger.info("New connection");
+});
+
+const routes = [
+    [/^\/about$/gi, about],
+    [/^\/image$/gi, staticImage],
+    [/^\/static/gi, staticFiles],
+    [/^\/$/gi, index]
+];
+
+server.on("request", (req, res) => {
+    const purl = url.parse(req.url);
+    req.URL = purl;
+    const query = purl.query;
+    const pathname = purl.pathname; // /about /iamge / /sljdf/sljfl
+    const params = querystring.parse(query);
+    let has_match = false;
+    routes.forEach(([regex, handler]) => {
+        if (has_match) return;
+        if (regex.test(pathname)) {
+            has_match = true;
+            handler(req, res);
+        }
+    });
+    if (!has_match) {
+        error404(req, res);
+    }
 });
 
 server.on("request", (req, res) => {
-  const purl = url.parse(req.url);
-  const query = purl.query;
-  const pathname = purl.pathname;
-  const params = querystring.parse(query);
-  switch (pathname) {
-    case "/about":
-      about(req, res);
-      break;
-    case "/image":
-      staticImage(req, res);
-      break;
-    case "/":
-      index(req, res);
-      break;
-    default:
-      error404(req, res);
-  }
-});
-
-server.on("request", (req, res) => {
-  console.log("New request");
+    console.log("New request");
 });
 
 server.on("listening", () => {
-  logger.info(`Server is started! Port: ${PORT}`);
+    logger.info(`Server is started! Port: ${PORT}`);
 });
 
 server.listen(PORT);
