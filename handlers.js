6e202be1b45f4b2002815fb883a8d7c0b74c0cb6
VladimirZhdanov <VladimirZhdanov1992@gmail.com>
2019-03-13 17:45:28 +0600

    Abirvalg

--- handlers.js
+++ handlers.js
@@ -1,6 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
+const serveFile = (path, req, res) => {
+    const fstream = fs.createReadStream(path);
+    fstream.pipe(res);
+    fstream.on("data", chunk => {
+        console.log(chunk);
+    });
+    fstream.on("end", () => {
+        res.end();
+    });
+    fstream.on("error", () => {
+        res.end("=(");
+    });
+};
+
 const about = (req, res) => {
     res.end("<h1>About</h1>");
 };
@@ -15,7 +29,6 @@ const error404 = (req, res) => {
 
 const staticImage = (req, res) => {
     console.log(req.URL.pathname);
-
     const fstream = fs.createReadStream(
         path.join(process.cwd(), "./static/images/image.jpg")
     );
@@ -33,22 +46,14 @@ const staticImage = (req, res) => {
 
 const staticFiles = (req, res) => {
     console.log(req.URL.pathname);
+    // Получаем абсолютный путь до файла на основании нашей текущей рабочей директории и пути из req.url
     abs_path = path.join(process.cwd(), req.URL.pathname);
+    // Запрашиваем информацию о файле
     fs.stat(abs_path, (err, fs_stats) => {
         if (err) {
             error404(req, res);
         } else {
-            const fstream = fs.createReadStream(abs_path);
-            fstream.pipe(res);
-            fstream.on("data", chunk => {
-                console.log(chunk);
-            });
-            fstream.on("end", () => {
-                res.end();
-            });
-            fstream.on("error", () => {
-                res.end("=(");
-            });
+            serveFile(abs_path, req, res);
         }
     });
 };
